424838c37f6b70eaa304762d65588c7db3e85867
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-09-20 21:46:12 +0100

    Start working on the client side "HomeManager" project. Add processor model, controller, and view to the project

---
+++ HomeManager/Controllers/PCBuild/ProcessorController.cs
@@ -0,0 +1,120 @@
+﻿using HomeManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HomeManager.Controllers.PCBuild
+{
+
+    public class ProcessorController : Controller
+    {
+        private readonly ILogger<ProcessorController> _logger;
+        private readonly IConfiguration _configure;
+        private readonly string apiBaseUrl;
+
+        public ProcessorController(ILogger<ProcessorController> logger, IConfiguration configuration)
+        {
+            this._logger = logger;
+            this._configure = configuration;
+
+            this.apiBaseUrl = this._configure.GetValue<string>("WebAPIBaseUrl");
+        }
+
+
+        // GET: Processor
+        public async Task<IActionResult> Index()
+        {
+            var processorsList = new List<Processor>();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/processors", this.apiBaseUrl)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    processorsList = JsonConvert.DeserializeObject<List<Processor>>(apiResponse);
+                }
+            }
+
+
+            return this.View(processorsList);
+        }
+
+        //// GET: Processor/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: Processor/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Processor/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Processor/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Processor/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Processor/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Processor/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ HomeManager/Models/Processor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeManager.Models
+{
+    public class Processor
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ProductCollection { get; set; }
+        public int NumberOfCores { get; set; }
+        public int NumberOfThreads { get; set; }
+        public int Cache { get; set; }
+        public int TDP { get; set; }
+        public decimal ProcessorBaseFrequency { get; set; }
+        public decimal Price { get; set; }
+        public string Link { get; set; }
+    }
+}
