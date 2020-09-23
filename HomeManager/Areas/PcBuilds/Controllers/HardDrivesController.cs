58191daaba34a434d60afe84e1b24c9226a7f2c2
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-09-23 18:04:37 +0100

    Add HardDrives (MVC) controller and views

---
+++ HomeManager/Areas/PcBuilds/Controllers/HardDrivesController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HomeManager.Areas.PcBuilds.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace HomeManager.Areas.PcBuilds.Controllers
+{
+    [Area("PcBuilds")]
+    public class HardDrivesController : Controller
+    {
+        private readonly ILogger<HardDrivesController> _logger;
+        private readonly IConfiguration _configure;
+        private readonly string apiBaseUrl;
+        private readonly string apiController = "harddrives";
+
+        public HardDrivesController(ILogger<HardDrivesController> logger, IConfiguration configuration)
+        {
+            this._logger = logger;
+            this._configure = configuration;
+
+            this.apiBaseUrl = this._configure.GetValue<string>("WebAPIBaseUrl");
+        }
+
+
+        // GET: HardDrives
+        public async Task<IActionResult> Index()
+        {
+            var hardDrive = new List<HardDrive>();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    hardDrive = JsonConvert.DeserializeObject<List<HardDrive>>(apiResponse);
+                }
+            }
+
+
+            return this.View(hardDrive);
+        }
+
+        // GET: HardDrives/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var hardDrive = new HardDrive();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    hardDrive = JsonConvert.DeserializeObject<HardDrive>(apiResponse);
+                }
+            }
+
+            return this.View(hardDrive);
+        }
+
+
+
+        // GET: HardDrives/Create
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        // POST: HardDrives/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(HardDrive model)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await httpClient.PostAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController), content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+        // GET: HardDrives/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var hardDrive = new HardDrive();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    hardDrive = JsonConvert.DeserializeObject<HardDrive>(apiResponse);
+                }
+            }
+
+            return this.View(hardDrive);
+        }
+
+        // POST: HardDrives/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, HardDrive model)
+        {
+            try
+            {
+                if (id != model.Id || model == null)
+                {
+                    return this.NotFound();
+                }
+
+                using (var httpClient = new HttpClient())
+                {
+                    model.Id = id;
+                    string json = JsonConvert.SerializeObject(model, Formatting.Indented);
+                    var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await httpClient.PutAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController), httpContent))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();    // returns object, todo: change response in api to return successfull message
+                        //ViewBag.Result = "Success";
+                        //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+        // GET: HardDrives/Delete/5
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var hardDrive = new HardDrive();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    hardDrive = JsonConvert.DeserializeObject<HardDrive>(apiResponse);
+                }
+            }
+
+            return this.View(hardDrive);
+        }
+
+        // POST: HardDrive/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id, HardDrive hardDrive = null)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (HttpResponseMessage response = await httpClient.DeleteAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+
+    }
+}
