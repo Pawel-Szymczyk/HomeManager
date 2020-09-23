5e6f1e1cfa7f2f7cd8a580e067b7a0e39c8bcd59
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-09-23 19:08:42 +0100

    Add RAM (MVC) controller and views

---
+++ HomeManager/Areas/PcBuilds/Controllers/RAMsController.cs
@@ -0,0 +1,189 @@
+﻿using HomeManager.Areas.PcBuilds.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeManager.Areas.PcBuilds.Controllers
+{
+    [Area("PcBuilds")]
+    public class RAMsController : Controller
+    {
+        private readonly ILogger<RAMsController> _logger;
+        private readonly IConfiguration _configure;
+        private readonly string apiBaseUrl;
+        private readonly string apiController = "rams";
+
+        public RAMsController(ILogger<RAMsController> logger, IConfiguration configuration)
+        {
+            this._logger = logger;
+            this._configure = configuration;
+
+            this.apiBaseUrl = this._configure.GetValue<string>("WebAPIBaseUrl");
+        }
+
+
+        // GET: RAMs
+        public async Task<IActionResult> Index()
+        {
+            var ramList = new List<RAM>();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ramList = JsonConvert.DeserializeObject<List<RAM>>(apiResponse);
+                }
+            }
+
+
+            return this.View(ramList);
+        }
+
+        // GET: RAMs/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var ram = new RAM();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ram = JsonConvert.DeserializeObject<RAM>(apiResponse);
+                }
+            }
+
+            return this.View(ram);
+        }
+
+
+
+        // GET: RAMs/Create
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        // POST: RAMs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(RAM model)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await httpClient.PostAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController), content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+        // GET: RAMs/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var ram = new RAM();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ram = JsonConvert.DeserializeObject<RAM>(apiResponse);
+                }
+            }
+
+            return this.View(ram);
+        }
+
+        // POST: RAMs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, RAM model)
+        {
+            try
+            {
+                if (id != model.Id || model == null)
+                {
+                    return this.NotFound();
+                }
+
+                using (var httpClient = new HttpClient())
+                {
+                    model.Id = id;
+                    string json = JsonConvert.SerializeObject(model, Formatting.Indented);
+                    var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await httpClient.PutAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController), httpContent))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();    // returns object, todo: change response in api to return successfull message
+                        //ViewBag.Result = "Success";
+                        //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+        // GET: RAMs/Delete/5
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var ram = new RAM();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ram = JsonConvert.DeserializeObject<RAM>(apiResponse);
+                }
+            }
+
+            return this.View(ram);
+        }
+
+        // POST: RAMs/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id, RAM ram = null)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (HttpResponseMessage response = await httpClient.DeleteAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+    }
+}
