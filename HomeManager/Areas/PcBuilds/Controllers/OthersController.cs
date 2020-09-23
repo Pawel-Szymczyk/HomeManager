9641ed6c56f8a79bba7cdd65c3e6a94bfc090333
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-09-23 18:44:23 +0100

    Add pcCase and Other (MVC) controllers and views

---
+++ HomeManager/Areas/PcBuilds/Controllers/OthersController.cs
@@ -0,0 +1,189 @@
+﻿using HomeManager.Areas.PcBuilds.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeManager.Areas.PcBuilds.Controllers
+{
+    [Area("PcBuilds")]
+    public class OthersController : Controller
+    {
+        private readonly ILogger<OthersController> _logger;
+        private readonly IConfiguration _configure;
+        private readonly string apiBaseUrl;
+        private readonly string apiController = "others";
+
+        public OthersController(ILogger<OthersController> logger, IConfiguration configuration)
+        {
+            this._logger = logger;
+            this._configure = configuration;
+
+            this.apiBaseUrl = this._configure.GetValue<string>("WebAPIBaseUrl");
+        }
+
+
+        // GET: Others
+        public async Task<IActionResult> Index()
+        {
+            var othersList = new List<Other>();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    othersList = JsonConvert.DeserializeObject<List<Other>>(apiResponse);
+                }
+            }
+
+
+            return this.View(othersList);
+        }
+
+        // GET: Others/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var other = new Other();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    other = JsonConvert.DeserializeObject<Other>(apiResponse);
+                }
+            }
+
+            return this.View(other);
+        }
+
+
+
+        // GET: Others/Create
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        // POST: Others/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Other model)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await httpClient.PostAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController), content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+        // GET: Others/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var other = new Other();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    other = JsonConvert.DeserializeObject<Other>(apiResponse);
+                }
+            }
+
+            return this.View(other);
+        }
+
+        // POST: Others/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Other model)
+        {
+            try
+            {
+                if (id != model.Id || model == null)
+                {
+                    return this.NotFound();
+                }
+
+                using (var httpClient = new HttpClient())
+                {
+                    model.Id = id;
+                    string json = JsonConvert.SerializeObject(model, Formatting.Indented);
+                    var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await httpClient.PutAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController), httpContent))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();    // returns object, todo: change response in api to return successfull message
+                        //ViewBag.Result = "Success";
+                        //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+        // GET: Others/Delete/5
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var other = new Other();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    other = JsonConvert.DeserializeObject<Other>(apiResponse);
+                }
+            }
+
+            return this.View(other);
+        }
+
+        // POST: Others/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id, Other other = null)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (HttpResponseMessage response = await httpClient.DeleteAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+    }
+}
---
+++ HomeManager/Areas/PcBuilds/Controllers/PCCasesController.cs
@@ -0,0 +1,189 @@
+﻿using HomeManager.Areas.PcBuilds.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeManager.Areas.PcBuilds.Controllers
+{
+    [Area("PcBuilds")]
+    public class PCCasesController : Controller
+    {
+        private readonly ILogger<PCCasesController> _logger;
+        private readonly IConfiguration _configure;
+        private readonly string apiBaseUrl;
+        private readonly string apiController = "pccases";
+
+        public PCCasesController(ILogger<PCCasesController> logger, IConfiguration configuration)
+        {
+            this._logger = logger;
+            this._configure = configuration;
+
+            this.apiBaseUrl = this._configure.GetValue<string>("WebAPIBaseUrl");
+        }
+
+
+        // GET: PCCases
+        public async Task<IActionResult> Index()
+        {
+            var pcCaseList = new List<PCCase>();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    pcCaseList = JsonConvert.DeserializeObject<List<PCCase>>(apiResponse);
+                }
+            }
+
+
+            return this.View(pcCaseList);
+        }
+
+        // GET: PCCases/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var pcCase = new PCCase();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    pcCase = JsonConvert.DeserializeObject<PCCase>(apiResponse);
+                }
+            }
+
+            return this.View(pcCase);
+        }
+
+
+
+        // GET: PCCases/Create
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        // POST: PCCases/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PCCase model)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await httpClient.PostAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController), content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+        // GET: PCCases/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var pcCase = new PCCase();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    pcCase = JsonConvert.DeserializeObject<PCCase>(apiResponse);
+                }
+            }
+
+            return this.View(pcCase);
+        }
+
+        // POST: PCCases/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, PCCase model)
+        {
+            try
+            {
+                if (id != model.Id || model == null)
+                {
+                    return this.NotFound();
+                }
+
+                using (var httpClient = new HttpClient())
+                {
+                    model.Id = id;
+                    string json = JsonConvert.SerializeObject(model, Formatting.Indented);
+                    var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await httpClient.PutAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController), httpContent))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();    // returns object, todo: change response in api to return successfull message
+                        //ViewBag.Result = "Success";
+                        //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+        // GET: PCCases/Delete/5
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var pcCase = new PCCase();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    pcCase = JsonConvert.DeserializeObject<PCCase>(apiResponse);
+                }
+            }
+
+            return this.View(pcCase);
+        }
+
+        // POST: PCCases/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id, PCCase pcCase = null)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (HttpResponseMessage response = await httpClient.DeleteAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+    }
+}
