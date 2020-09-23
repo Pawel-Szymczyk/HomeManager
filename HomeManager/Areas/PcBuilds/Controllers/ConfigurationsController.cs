4f51fff2ad808a1fa920de3d189c0e37b7a5fa5a
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-09-23 20:24:28 +0100

    Add Configuration MVC (not finished)

---
+++ HomeManager/Areas/PcBuilds/Controllers/ConfigurationsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HomeManager.Areas.PcBuilds.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace HomeManager.Areas.PcBuilds.Controllers
+{
+    [Area("PcBuilds")]
+    public class ConfigurationsController : Controller
+    {
+        private readonly ILogger<ConfigurationsController> _logger;
+        private readonly IConfiguration _configure;
+        private readonly string apiBaseUrl;
+        private readonly string apiController = "pcbuilds";
+
+        public ConfigurationsController(ILogger<ConfigurationsController> logger, IConfiguration configuration)
+        {
+            this._logger = logger;
+            this._configure = configuration;
+
+            this.apiBaseUrl = this._configure.GetValue<string>("WebAPIBaseUrl");
+        }
+
+        // GET: Configurations
+        public async Task<IActionResult> Index()
+        {
+
+            var configurationsList = new List<Configuration>();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    configurationsList = JsonConvert.DeserializeObject<List<Configuration>>(apiResponse);
+                }
+            }
+
+
+            return this.View(configurationsList);
+        }
+
+        // GET: Configurations/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var configuration = new Configuration();
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    configuration = JsonConvert.DeserializeObject<Configuration>(apiResponse);
+                }
+            }
+
+            return this.View(configuration);
+        }
+    }
+}
---
+++ HomeManager/Areas/PcBuilds/Models/Configuration.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeManager.Areas.PcBuilds.Models
+{
+    public class Configuration
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        [Display(Name = "Total price")]
+        [DataType(DataType.Currency)]
+        public decimal TotalPrice { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ModifiedDate { get; set; }
+
+
+
+        //[ForeignKey("CPUWatercooler")]
+        //public Guid? CPUWatercoolerId { get; set; }
+        //public virtual CPUWatercooler CPUWatercooler { get; set; }
+        public CPUWatercooler CPUWatercooler { get; set; }
+
+        //[ForeignKey("Fan")]
+        //public Guid? FanId { get; set; }
+        //public virtual Fan Fan { get; set; }
+        public Fan Fan { get; set; }
+
+        //[ForeignKey("GraphicsCard")]
+        //public Guid? GraphicsCardId { get; set; }
+        //public virtual GraphicsCard GraphicsCard { get; set; }
+        public GraphicsCard GraphicsCard { get; set; }
+
+        //[ForeignKey("HardDrive")]
+        //public Guid? HardDrivedId { get; set; }
+        //public virtual HardDrive HardDrive { get; set; }
+        public HardDrive HardDrive { get; set; }
+
+        //[ForeignKey("Motherboard")]
+        //public Guid? MotherboardId { get; set; }
+        //public virtual Motherboard Motherboard { get; set; }
+        public Motherboard Motherboard { get; set; }
+
+        //[ForeignKey("Other")]
+        //public Guid? OtherId { get; set; }
+        //public virtual Other Other { get; set; }
+        public Other Other { get; set; }
+
+        //[ForeignKey("PCCase")]
+        //public Guid? PCCaseId { get; set; }
+        //public virtual PCCase PCCase { get; set; }
+        public PCCase PCCase { get; set; }
+
+        //[ForeignKey("PowerSupply")]
+        //public Guid? PowerSupplyId { get; set; }
+        //public virtual PowerSupply PowerSupply { get; set; }
+        public PowerSupply PowerSupply { get; set; }
+
+        //[ForeignKey("Processor")]
+        //public Guid? ProcessorId { get; set; }
+        //public virtual Processor Processor { get; set; }
+        public Processor Processor { get; set; }
+
+        //[ForeignKey("RAM")]
+        //public Guid? RAMId { get; set; }
+        //public virtual RAM RAM { get; set; }
+        public RAM RAM { get; set; }
+    }
+}
