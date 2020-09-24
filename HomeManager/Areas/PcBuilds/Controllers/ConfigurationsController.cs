37dcd2fbdf7f319d686a442507b267217e6412f0
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-09-24 19:14:11 +0100

    add configuration controller and 2 views create adn details

--- HomeManager/Areas/PcBuilds/Controllers/ConfigurationsController.cs
+++ HomeManager/Areas/PcBuilds/Controllers/ConfigurationsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using HomeManager.Areas.PcBuilds.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -60,5 +61,78 @@ namespace HomeManager.Areas.PcBuilds.Controllers
 
             return this.View(configuration);
         }
+
+        // GET: Configurations/Create
+        public IActionResult Create()
+        {
+            // get list of processors
+
+
+            return this.View();
+        }
+
+        // POST: Configurations/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Configuration model)
+        {
+            try
+            {
+                //Configuration configuration = new Configuration
+                //{
+                //    Id = "",
+                //    CPUWatercooler = "",
+                //    CreatedDate = "",
+                //    Description = "",
+                //    Fan = "",
+                //    GraphicsCard ="",
+                //    HardDrive = "",
+                //    ModifiedDate = "",
+                //    Motherboard = "",
+                //    Other ="",
+                //    PCCase = "",
+                //    PowerSupply ="",
+                //    Processor ="",
+                //    RAM = "",
+                //    TotalPrice = ""
+                //}
+
+                var configuration = new {
+                    cpuWatercoolerId = model.CPUWatercooler.Id,
+                    description = model.Description,
+                    fanId = model.Fan.Id,
+                    graphicsCardId = model.GraphicsCard.Id,
+                    hardDrivedId = model.HardDrive.Id,
+                    motherboardId = model.Motherboard.Id,
+                    otherId = model.Other.Id,
+                    pcCaseId = model.PCCase.Id,
+                    powerSupplyId = model.PowerSupply.Id,
+                    processorId = model.Processor.Id,
+                    ramId = model.RAM.Id,
+                    totalPrice = model.TotalPrice
+                };
+
+                using (var httpClient = new HttpClient())
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(configuration), Encoding.UTF8, "application/json");
+
+                    using (HttpResponseMessage response = await httpClient.PostAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiController), content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        //receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                }
+
+                return this.RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+
+
+
+
     }
 }
