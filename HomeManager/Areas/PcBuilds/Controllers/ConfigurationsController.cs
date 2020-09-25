0bdfa45c21fed61b947d06640bee3566a401c450
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-09-25 21:57:04 +0100

    Update pc build create view and controller

--- HomeManager/Areas/PcBuilds/Controllers/ConfigurationsController.cs
+++ HomeManager/Areas/PcBuilds/Controllers/ConfigurationsController.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using HomeManager.Areas.PcBuilds.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using HomeManager.Areas.PcBuilds.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace HomeManager.Areas.PcBuilds.Controllers
 {
@@ -64,53 +66,94 @@ namespace HomeManager.Areas.PcBuilds.Controllers
         }
 
         // GET: Configurations/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            // get list of processors
+            // References: https://www.newtonsoft.com/json/help/html/ToObjectComplex.htm
+
+            
+
+            Components components = new Components();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}", this.apiBaseUrl, this.apiComponentsController)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+
+                    var jObject = JObject.Parse(apiResponse);
 
 
-            return this.View();
+                    JArray cpuWatercoolers = (JArray)jObject["cpuWatercoolers"];
+                    IList<CPUWatercooler> cpuWatercoolersList = cpuWatercoolers.ToObject<IList<CPUWatercooler>>();
+                    components.CPUWatercoolers = new List<CPUWatercooler>(cpuWatercoolersList);
+
+                    JArray fans = (JArray)jObject["fans"];
+                    IList<Fan> fansList = fans.ToObject<IList<Fan>>();
+                    components.Fans = new List<Fan>(fansList);
+
+                    JArray graphicsCards = (JArray)jObject["graphicsCards"];
+                    IList<GraphicsCard> graphicsCardsList = graphicsCards.ToObject<IList<GraphicsCard>>();
+                    components.GraphicsCards = new List<GraphicsCard>(graphicsCardsList);
+
+                    JArray hardDrives = (JArray)jObject["hardDrives"];
+                    IList<HardDrive> hardDrivesList = hardDrives.ToObject<IList<HardDrive>>();
+                    components.HardDrives = new List<HardDrive>(hardDrivesList);
+
+                    JArray motherboards = (JArray)jObject["motherboards"];
+                    IList<Motherboard> motherboardsList = motherboards.ToObject<IList<Motherboard>>();
+                    components.Motherboards = new List<Motherboard>(motherboardsList);
+
+                    JArray others = (JArray)jObject["others"];
+                    IList<Other> othersList = others.ToObject<IList<Other>>();
+                    components.Others = new List<Other>(othersList);
+
+                    JArray pcCases = (JArray)jObject["pcCases"];
+                    IList<PCCase> pcCasesList = pcCases.ToObject<IList<PCCase>>();
+                    components.PCCases = new List<PCCase>(pcCasesList);
+
+                    JArray powerSupplies = (JArray)jObject["powerSupplies"];
+                    IList<PowerSupply> powerSuppliesList = powerSupplies.ToObject<IList<PowerSupply>>();
+                    components.PowerSupplies = new List<PowerSupply>(powerSuppliesList);
+
+                    JArray processors = (JArray)jObject["processors"];
+                    IList<Processor> processorsList = processors.ToObject<IList<Processor>>();
+                    components.Processors = new List<Processor>(processorsList);
+
+                    JArray rams = (JArray)jObject["rams"];
+                    IList<RAM> ramsList = rams.ToObject<IList<RAM>>();
+                    components.RAMs = new List<RAM>(ramsList);
+
+
+                }
+            }
+
+            return this.View(components);
         }
+
+
 
         // POST: Configurations/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Configuration model)
+        public async Task<IActionResult> Create(Components model)
         {
             try
             {
-                //Configuration configuration = new Configuration
-                //{
-                //    Id = "",
-                //    CPUWatercooler = "",
-                //    CreatedDate = "",
-                //    Description = "",
-                //    Fan = "",
-                //    GraphicsCard ="",
-                //    HardDrive = "",
-                //    ModifiedDate = "",
-                //    Motherboard = "",
-                //    Other ="",
-                //    PCCase = "",
-                //    PowerSupply ="",
-                //    Processor ="",
-                //    RAM = "",
-                //    TotalPrice = ""
-                //}
 
-                var configuration = new {
-                    cpuWatercoolerId = model.CPUWatercooler.Id,
-                    description = model.Description,
-                    fanId = model.Fan.Id,
-                    graphicsCardId = model.GraphicsCard.Id,
-                    hardDrivedId = model.HardDrive.Id,
-                    motherboardId = model.Motherboard.Id,
-                    otherId = model.Other.Id,
-                    pcCaseId = model.PCCase.Id,
-                    powerSupplyId = model.PowerSupply.Id,
-                    processorId = model.Processor.Id,
-                    ramId = model.RAM.Id,
-                    totalPrice = model.TotalPrice
+                var configuration = new
+                {
+                    cpuWatercoolerId = model.SelectedCPUWatercoolerId,
+                    description = model.Configuration.Description,
+                    fanId = model.SelectedFanId,
+                    graphicsCardId = model.SelectedGraphicsCardId,
+                    hardDrivedId = model.SelectedHardDriveId,
+                    motherboardId = model.SelectedMotherboardId,
+                    otherId = model.SelectedOtherId,
+                    pcCaseId = model.SelectedPCCaseId,
+                    powerSupplyId = model.SelectedPowerSupplyId,
+                    processorId = model.SelectedProcessorId,
+                    ramId = model.SelectedRAMId,
+                    totalPrice = model.Configuration.TotalPrice // to do
                 };
 
                 using (var httpClient = new HttpClient())
@@ -132,6 +175,22 @@ namespace HomeManager.Areas.PcBuilds.Controllers
             }
         }
 
+
+        // GET: Fans/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            //var fan = new Fan();
+            //using (var httpClient = new HttpClient())
+            //{
+            //    using (HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}/{2}", this.apiBaseUrl, this.apiController, id)))
+            //    {
+            //        string apiResponse = await response.Content.ReadAsStringAsync();
+            //        fan = JsonConvert.DeserializeObject<Fan>(apiResponse);
+            //    }
+            //}
+
+            return this.View();
+        }
 
 
 
---
+++ HomeManager/Areas/PcBuilds/Models/Components.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeManager.Areas.PcBuilds.Models
+{
+    public class Components
+    {
+        public Configuration Configuration { get; set; }
+
+        public List<CPUWatercooler> CPUWatercoolers { get; set; }
+        public List<Fan> Fans { get; set; }
+        public List<GraphicsCard> GraphicsCards { get; set; }
+        public List<HardDrive> HardDrives { get; set; }
+        public List<Motherboard> Motherboards { get; set; }
+        public List<Other> Others { get; set; }
+        public List<PCCase> PCCases { get; set; }
+        public List<PowerSupply> PowerSupplies { get; set; }
+        public List<Processor> Processors { get; set; }
+        public List<RAM> RAMs { get; set; }
+
+
+        // view
+        public string SelectedCPUWatercoolerId { get; set; }
+        public string SelectedFanId { get; set; }
+        public string SelectedGraphicsCardId { get; set; }
+        public string SelectedHardDriveId { get; set; }
+        public string SelectedMotherboardId { get; set; }
+        public string SelectedOtherId { get; set; }
+        public string SelectedPCCaseId { get; set; }
+        public string SelectedPowerSupplyId { get; set; }
+        public string SelectedProcessorId { get; set; }
+        public string SelectedRAMId { get; set; }
+    }
+}
