ab10761284fb62cc8f09945bed20bdd385a58210
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-10-12 11:21:45 +0100

    - move navigation script to separate js file - add two new navigation background images - edit styling for navigation bar and footer

---
+++ HomeManager/wwwroot/js/navigation.js
@@ -0,0 +1,20 @@
+﻿window.onscroll = function () { myFunction() };
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function openNav() {
+    document.getElementById("overlay-nav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("overlay-nav").style.height = "0%";
+}
