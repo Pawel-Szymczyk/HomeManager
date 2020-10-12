f3da349cd96337f2a5333318ef80a60ccdc143c2
Pawel-Szymczyk <szymczyk.pawel@yahoo.co.uk>
2020-10-12 08:46:50 +0100

    - update all pages banners with new styling - update common css file by removing old banner style - create banner js file responsible for loading banner image depends on the time frame of the day - move banner code to layout to ensure that script is loaded globally for all views

---
+++ HomeManager/wwwroot/js/banner.js
@@ -0,0 +1,40 @@
+﻿
+
+window.onload = function () {
+    loadBanner();
+}
+
+
+// Function responsible for loading different background image depends on time frame.
+// e.g. between 5am and 10am function loads "morning" background.
+function loadBanner() {
+    let urlString;
+    let banner = document.getElementById("pc-builder-banner");
+    let curHr = new Date().getHours();
+
+    if (curHr >= 5 && curHr < 10) {
+        urlString = 'url(/lib/images/morning.png)';
+        banner.style.backgroundImage = urlString;
+    } else if (curHr >= 10 && curHr < 12) {
+        urlString = 'url(/lib/images/late_morning.png)';
+        banner.style.backgroundImage = urlString;
+    } else if (curHr >= 12 && curHr < 15) {
+        urlString = 'url(/lib/images/afternoon.png)';
+        banner.style.backgroundImage = urlString;
+    } else if (curHr >= 15 && curHr < 18) {
+        urlString = 'url(/lib/images/late_afternoon.png)';
+        banner.style.backgroundImage = urlString;
+    } else if (curHr >= 18 && curHr < 20) {
+        urlString = 'url(/lib/images/evening.png)';
+        banner.style.backgroundImage = urlString;
+    } else if (curHr >= 20 && curHr < 22) {
+        urlString = 'url(/lib/images/late_evening.png)';
+        banner.style.backgroundImage = urlString;
+    } else if (curHr >= 22 && curHr < 24) {
+        urlString = 'url(/lib/images/night.png)';
+        banner.style.backgroundImage = urlString;
+    } else {
+        urlString = 'url(/lib/images/late_night.png)';
+        banner.style.backgroundImage = urlString;
+    }
+}
